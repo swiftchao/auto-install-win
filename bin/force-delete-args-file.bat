6eb41240a93fe134c6118f695ac08200380eeb9c
chaofei <chaofeibest@163.com>
2017-07-06 01:21:23 +0800

    修改强制删除文件夹的函数

--- bin/force-delete-args-file.bat
+++ bin/force-delete-args-file.bat
@@ -15,19 +15,30 @@ if not exist "%AUTO_INSTALL_HOME%\conf\config.bat" goto noConfig
 set "CONFIG=%AUTO_INSTALL_HOME%\conf\config.bat"
 call %CONFIG%
 
+:killArgsProcessByFile
+if ""%1""=="""" goto killArgsProcessByFileEnd
+for /f "eol=; tokens=2 delims= " %%i in ('tasklist /v ^|find /i %1') do (
+  echo %%i
+  taskkill /PID %%i /T
+)
+goto :eof
+:killArgsProcessByFileEnd
+
 :forceDeleteArgsFile
 :forceDeleteArgsFileStart
-if "%1" == "" goto forceDeleteArgsFileEnd
-if exist “%1” (
+if ""%1""=="""" goto forceDeleteArgsFileEnd
+title Force Delete %1
+if exist %1 (
   title Force Delete %1...
   rd /s /q %1
   if exist %1 (
+    ping -n 5 127.0.0.1 >nul
     goto forceDeleteArgsFileStart
   ) else (
-    goto forceDeleteArgsFileEnd
+    goto :eof
   )
 )
-:forceDeleteArgsFileEnd
+
 
 :noConfig
 cd "%CURRENT_DIR%"

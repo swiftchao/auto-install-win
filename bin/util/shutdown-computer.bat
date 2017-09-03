f0df6b134d77b69dac9b6e5775259ebc3e35e097
chaofei <chaofeibest@163.com>
2017-09-04 00:47:56 +0800

    新增关机和重启电脑的脚本

---
+++ bin/util/shutdown-computer.bat
@@ -0,0 +1,11 @@
+@echo off
+rem ---------------------------------------------------------------------------
+rem Shutdown computer
+rem
+rem File Name: shutdown-computer.bat
+rem Author: chaofei
+rem E-Mail: chaofeibest@163.com
+rem Since: 2016-6-16 00:45:25
+rem ---------------------------------------------------------------------------
+
+C:\Windows\System32\shudown.exe -P

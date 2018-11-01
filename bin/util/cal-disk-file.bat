3124f746dd1205ee7c51b2c8bda8d335ae25d4d8
chaofei <chaofeibest@163.com>
2018-11-01 16:14:51 +0800

    新增计算磁盘文件和文件大小的脚本

---
+++ bin/util/cal-disk-file.bat
@@ -0,0 +1,82 @@
+@echo off&setlocal enabledelayedexpansion
+set DISK=E
+echo 正在查询%DISK%盘文件的数量，请等待...
+dir /s %DISK%: >files.txt
+    set /a n=2
+    for /f "delims=" %%a in (files.txt) do (
+      for /l %%b in (%n%,-1,1) do (
+        if %%b neq 1 (
+          set /a a=%%b-1
+          for %%c in (!a!) do set "str%%b=!str%%c!"
+          ) else (
+          set "str%%b=%%a" 
+        )
+      )
+    )
+set aaa=!str%n%!
+set ccc=%aaa:~8,12%
+echo 【%DISK%盘文件数量为%ccc%】
+pause
+echo 正在计算你%DISK%盘文件的大小，请等待...
+set bbb=%aaa:~20,-2%
+set "bbb=%bbb:,=%"
+set "str1=%bbb: =%"
+set str2=1073741824
+set u=2
+for %%i in (str1 str2) do if "!%%i:~,1!" == "-" set /a d+=1
+if "%d%" == "1" (set d=-) else set "d="
+set l=00000000&for /l %%i in (1 1 7) do set "l=!l!!l!"
+set "var=4096 2048 1024 512 256 128 64 32 16 8 4 2 1"
+for /l %%i in (1 1 2) do (
+    set "str%%i=!str%%i:-=!"
+    set /a "n=str%%i_2=0"
+    for %%a in (!str%%i:.^= !) do (
+        set /a n+=1
+        set s=s%%a&set str%%i_!n!=0
+        for %%b in (%var%) do if "!S:~%%b!" neq "" set/a str%%i_!n!+=%%b&set "S=!S:~%%b!"
+        set /a len%%i+=str%%i_!n!
+    )
+        set str%%i=!str%%i:.=!
+)
+if !str1_2! gtr !str2_2! (set /a len2+=str1_2-str2_2) else set /a len1+=str2_2-str1_2
+for /l %%i in (1 1 2) do (
+    set str%%i=!str%%i!!l!
+    for %%j in (!len%%i!) do set " str%%i=!str%%i:~,%%j!"
+)
+for /f "tokens=* delims=0" %%i in ("!str2!") do set s=%%i&set "str2=0%%i"
+set len2=1
+for %%j in (%var%) do if "!S:~%%j!" neq "" set/a len2+=%%j&set "S=!S:~%%j!"
+set /a len=len2+1
+if !len1! lss !len2! set len1=!len2!&set "str1=!l:~-%len2%,-%len1%!!str1!"
+set /a len1+=u&set str1=0!str1!!l:~,%u%!
+set str=!str1:~,%len2%!
+set "i=0000000!str2!"&set /a Len_i=Len2+7
+for /l %%i in (1 1 9) do (
+    set "T=0"
+    for /l %%j in (8 8 !Len_i!) do (
+        set /a "T=1!i:~-%%j,8!*%%i+T"
+        set Num%%i=!T:~-8!!Num%%i!&set /a "T=!T:~,-8!-%%i"
+    )
+    set Num%%i=!T!!Num%%i!
+    set "Num%%i=0000000!Num%%i:~-%Len%!"
+)
+for /L %%a in (!len2! 1 !Len1!) do (
+    set "str=!L!!str!!str1:~%%a,1!"
+    set "str=!str:~-%Len%!"
+    if "!str!" geq "!str2!" (
+       set M=1&set i=0000000!str!
+       for /l %%i in (2 1 9) do if "!i!" geq "!Num%%i!" set "M=%%i"
+           set sun=!sun!!M!&set str=&set T=0
+           for %%i in (!M!) do (
+               for /l %%j in (8 8 !Len_i!) do (
+                   set /a "T=3!i:~-%%j,8!-1!Num%%i:~-%%j,8!-!T:~,1!%%2"
+                   set "str=!T:~1!!str!"
+               )
+           )
+    ) else set sun=!sun!0
+)
+     set sun=!sun:~,-%u%!.!sun:~-%u%!
+echo 【%DISK%盘文件大小为%d%!sun!GB】
+echo 按任意键退出
+pause>nul
+exit 

54d7f7c18341526882f9eaa0f7b4352a30e2e984
chaofei <chaofeibest@163.com>
2018-11-28 15:21:25 +0800

    新增hashfile.bat计算文件的md5和sha1以及shar256的值，新增使用说明

--- bin/util/hashfile.bat
+++ bin/util/hashfile.bat
@@ -1,5 +1,9 @@
 @echo OFF
- 
+
+@rem 方式一：打开文件夹，在地址栏输入 shell:sendto 打开了“发送到”目录，将hashfile.bat文件复制到此目录。
+@rem 经过上面两步，就可以实现选中一个或多个文件，右键单击，选择发送到(N)再选择hashfile.bat，就会自动计算校验和"
+@rem 方式二: 起一个命令行 cmd 执行 hashfile.bat 文件1 文件2 ...
+
 :LOOP
  
 	set index=%1

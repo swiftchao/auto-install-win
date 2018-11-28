bcea7dfe234bb9f50f9be3e3bd9c6243543109ad
chaofei <chaofeibest@163.com>
2018-11-28 15:47:09 +0800

    新增使用说明输出，并解决中文输出的乱码问题

--- bin/util/hashfile.bat
+++ bin/util/hashfile.bat
@@ -1,8 +1,7 @@
 @echo OFF
 
-@rem 方式一：打开文件夹，在地址栏输入 shell:sendto 打开了“发送到”目录，将hashfile.bat文件复制到此目录。
-@rem 经过上面两步，就可以实现选中一个或多个文件，右键单击，选择发送到(N)再选择hashfile.bat，就会自动计算校验和"
-@rem 方式二: 起一个命令行 cmd 执行 hashfile.bat 文件1 文件2 ...
+@rem 声明采用UTF-8编码 解决输出中文乱码问题
+chcp 65001
 
 :LOOP
  
@@ -27,5 +26,8 @@
 :END
  
 echo "Checksum ends."
+echo "方式一：打开文件夹，在地址栏输入 shell:sendto 打开了发送到目录，将hashfile.bat文件复制到此目录。"
+echo "  经过上面两步，就可以实现选中一个或多个文件，右键单击，选择发送到(N)再选择hashfile.bat，就会自动计算校验和"
+echo "方式二: 起一个命令行 cmd 执行 hashfile.bat 文件1 文件2 ..."
  
 pause

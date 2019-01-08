f182aca24e277ca46cb633bec1f77fb43ad1576e
chaofei <chaofeibest@163.com>
2019-01-08 14:26:15 +0800

    新增可以上插网线，也可以连wifi的脚本

---
+++ bin/util/internet-and-intranet.bat
@@ -0,0 +1,36 @@
+﻿@echo off
+
+route print 
+
+pause
+
+@echo off
+
+echo "Please check your physical network and wireless network are ok now ."
+echo "Restart compute,please type 1."
+echo "Plug and unplug the physical cable ,please type 2."
+echo "Thanks"
+
+choice /c 12 /m:
+
+if errorlevel 2 (
+	route delete 0.0.0.0 mask 0.0.0.0 172.1.180.254 
+	echo "           "
+	echo "           "
+	echo "finished!!!"
+	echo "           "
+	echo "           "
+	
+)
+if errorlevel 1 ( 
+	route delete 0.0.0.0 mask 0.0.0.0 172.1.180.254
+	echo "finished!!!"
+	route add 10.0.0.0 mask 255.0.0.0 172.1.180.254
+	echo "           "           
+	echo "           " 
+	echo "finished!!!"
+	echo "           " 
+	echo "           " 
+)
+
+cmd.exe

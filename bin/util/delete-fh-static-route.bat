7830ac960bd2d5afe3be2366897f2b3335c458fd
chaofei <fchao@fiberhome.com>
2022-12-06 20:20:41 +0800

    增加静态路由脚本

---
+++ bin/util/delete-fh-static-route.bat
@@ -0,0 +1,9 @@
+route DELETE 192.168.100.0
+route DELETE 172.16.62.0
+route DELETE 10.121.0.0
+route DELETE 10.127.0.0
+route DELETE 10.126.0.0
+route DELETE 10.128.0.0
+route DELETE 10.171.0.0
+route DELETE 10.16.0.0
+route print | findstr  "172.24.12.1"

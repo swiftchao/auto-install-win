3b44dec206e4ea7ebbf505863f3935046b10b540
chaofei <fchao@fiberhome.com>
2024-04-17 13:55:59 +0800

    【route】更新路由

--- bin/util/delete-fh-static-route.bat
+++ bin/util/delete-fh-static-route.bat
@@ -1,4 +1,6 @@
 route DELETE 192.168.100.0
+route DELETE 192.168.8.0
+route DELETE 192.168.10.0
 route DELETE 172.16.62.0
 route DELETE 10.121.0.0
 route DELETE 10.127.0.0
@@ -6,4 +8,7 @@ route DELETE 10.126.0.0
 route DELETE 10.128.0.0
 route DELETE 10.171.0.0
 route DELETE 10.16.0.0
-route print | findstr  "172.24.12.1"
+route DELETE 10.7.0.0
+route DELETE 10.132.0.0
+route DELETE 10.17.0.0
+route print | findstr  "172.29.6.202"
